91d28d10d8865498ef99a9fbb11e75e537741cfa
CodeLearningSlayer <rubtsov.maks2015@yandex.ru>
2022-06-14 16:15:11 +0300

    One page left

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+//при переходе toggleClass к названию header'a, чтоб менять фоновую картинку
 
 
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.testimonials .swiper', {
     modules:[Navigation],
     direction: "horizontal",
     loop: true,
@@ -11,6 +12,16 @@ const swiper = new Swiper('.swiper', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
+    }
+});
+
+const singlePlaceSlider = new Swiper('.single-place.swiper, .double-place.swiper, .twin-place.swiper', {
+    modules:[Pagination],
+    direction: "horizontal",
+    loop: true,
+    centeredSlides: true,
+    pagination:{
+        el: '.swiper-pagination',
     }
 });
 
