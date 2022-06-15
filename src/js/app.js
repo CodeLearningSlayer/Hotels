2b1bb7af8ddf896cc33753211f50796d614bf6b5
CodeLearningSlayer <rubtsov.maks2015@yandex.ru>
2022-06-15 14:32:14 +0300

    Start responsive

--- src/js/app.js
+++ src/js/app.js
@@ -25,8 +25,30 @@ const singlePlaceSlider = new Swiper('.single-place.swiper, .double-place.swiper
     }
 });
 
+if (document.querySelector(".scroll__btn"))
+{
+    document.querySelector(".scroll__btn").addEventListener("click", ()=>{
+        document.getElementById("scrollTo").scrollIntoView({
+            behavior:"smooth",
+            block:"start",
+        })
+    });
+}
 
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
 
+if (open && modal_container && close)
+{
+    open.addEventListener("click", ()=>{
+        modal_container.classList.add("show");
+    });
+    
+    close.addEventListener("click", ()=>{
+        modal_container.classList.remove("show");
+    });
+}
 
 
 
